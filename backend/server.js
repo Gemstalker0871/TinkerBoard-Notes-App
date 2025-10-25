b832da7263154cca9bab6d2682365839ff27e038
Sam <sambhavchugh2004@gmail.com>
2025-10-26 02:25:23 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,26 @@ import 'dotenv/config'
 import notesRoutes from "./routes/notesRoutes.routes.js"
 import { connectDB } from "./config/db.js"
 import rateLimiter from "./middleware/rateLimiter.middleware.js"
+import path from "path"
+import { fileURLToPath } from 'url';
 import cors from 'cors'
 
 const PORT = process.env.PORT
 
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const app = express()
 
-app.use(cors())
+if(process.env.NODE_ENV !== "production")
+app.use(cors({
+    origin: "http://localhost:5173",
+}))
 
-app.get('/', (req, res) => res.send("Api working"))
+
 app.use("/api/notes", express.json(), rateLimiter ,notesRoutes)
 
 
@@ -31,6 +41,20 @@ app.use("/api/notes", express.json(), rateLimiter ,notesRoutes)
 // app.delete("/api/notes/:id", (req, res) => {
 //     res.status(201).json({message:"Post deleted successfully"})
 // })
+
+
+if (process.env.NODE_ENV === "production") {
+  const distPath = path.resolve(__dirname, "../frontend/dist");
+
+  app.use(express.static(distPath));
+
+  app.get(/^\/(?!api).*/, (req, res) => {
+
+    res.sendFile(path.join(distPath, "index.html"));
+  });
+}
+
+
 connectDB().then(() => {
     app.listen(PORT, ()=> {
     console.log(`Server started on port ${PORT}`);
